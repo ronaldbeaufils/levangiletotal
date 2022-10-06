734febfbbbdee60c07b100afc269149a47fd4ef7
Ronaldbeaufils <beaufilsron@gmail.com>
2022-10-06 14:59:08 -0400

    bunch of changes

--- script/contact-mobile-action.js
+++ script/contact-mobile-action.js
@@ -2,7 +2,7 @@ if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigat
   // true for mobile device
   console.log("mobile device");
   var heade = document.getElementById('head');
-  heade.innerHTML = "<link rel='stylesheet' href='style/scrollbar.css'><br><link rel='stylesheet' href='style/texte-mobile.css'><br><link rel='stylesheet' href='style/general-mobile.css'><br><link rel='stylesheet' href='style/mobile-actualites.css'><br><title>Actualites</title>"
+  heade.innerHTML = "<link rel='stylesheet' href='style/scrollbar.css'><br><link rel='stylesheet' href='style/texte-mobile.css'><br><link rel='stylesheet' href='style/general-mobile.css'><br><link rel='stylesheet' href='style/contact-mobile.css'><br><title>Contact</title>"
 }else{
   // false for not mobile device
   console.log("not mobile device");
