a618d599928e965a58a095e6f7bc2df20b1bf825
Ronaldbeaufils <beaufilsron@gmail.com>
2022-10-06 17:55:37 -0400

    tested mobile veideo change

---
+++ script/videoElements/videoElement2-mobile.js
@@ -0,0 +1,9 @@
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+  // true for mobile device
+  console.log("mobile device");
+  var heade = document.getElementById('head');
+  heade.innerHTML = "<link rel='stylesheet' href='../style/scrollbar.css'><br><br><link rel='stylesheet' href='../style/general-mobile.css'><br><link rel='stylesheet' href='../style/videoElements/video1-mobile.css'<br><title>Voeux pour 2021</title>"
+}else{
+  // false for not mobile device
+  console.log("not mobile device");
+}
