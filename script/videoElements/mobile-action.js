be8b9128195d8303d3ebdb06ca89b93fd284898f
Ronaldbeaufils <beaufilsron@gmail.com>
2022-10-07 02:54:35 -0400

    a BUNCH of changes

---
+++ script/videoElements/mobile-action.js
@@ -0,0 +1,9 @@
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+  // true for mobile device
+  console.log("mobile device");
+  var heade = document.getElementById('head');
+  heade.innerHTML = "<link rel='stylesheet' href='style/scrollbar.css'><br><link rel='stylesheet' href='style/texte-mobile.css'><br><link rel='stylesheet' href='style/general-mobile.css'><br><title>Accueil</title>"
+}else{
+  // false for not mobile device
+  console.log("not mobile device");
+}
--- script/videoElements/videoElement1-mobile.js
+++ script/videoElements/videoElement1-mobile.js
@@ -2,7 +2,7 @@ if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigat
   // true for mobile device
   console.log("mobile device");
   var heade = document.getElementById('head');
-  heade.innerHTML = "<link rel='stylesheet' href='../style/scrollbar.css'><br><br><link rel='stylesheet' href='../style/general-mobile.css'><br><link rel='stylesheet' href='../style/videoElements/video1-mobile.css'<br><title>Kijan 2022 ap ye pou ou?</title>"
+  heade.innerHTML = "<link rel='stylesheet' href='../style/scrollbar.css'><br><br><link rel='stylesheet' href='../style/general-mobile.css'><br><link rel='stylesheet' href='../style/videoElements/video-mobile.css'><br><link rel='icon' href='../style/images/icon.ico' type='image/icon type'><br><title>Kijan 2022 ap ye pou ou?</title>"
 }else{
   // false for not mobile device
   console.log("not mobile device");
