6e8de237b305c09f97ebf8c71a41d900c5d66849
Shogun128 <69772843+Shogun128@users.noreply.github.com>
2023-09-05 13:08:45 -0400

    update for the website (3/4)

---
+++ script/videoElements/videoElement67-mobile.js
@@ -0,0 +1,9 @@
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+  // true for mobile device
+  console.log("mobile device");
+  var heade = document.getElementById('head');
+  heade.innerHTML = "<link rel='stylesheet' href='../style/scrollbar.css'><br><br><link rel='stylesheet' href='../style/general-mobile.css'><br><link rel='stylesheet' href='../style/videoElements/video-mobile.css'><br><link rel='icon' href='../style/images/icon.ico' type='image/icon type'><br><title>Je puis tout par celui qui me fortifie</title>"
+}else{
+  // false for not mobile device
+  console.log("not mobile device");
+}
