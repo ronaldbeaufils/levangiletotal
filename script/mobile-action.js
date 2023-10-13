c4cb227276687a1ac960fe8a9a8f80769e02a757
shogun <beaufilslegrand@gmail.com>
2023-10-13 06:46:22 -0400

    test

--- script/mobile-action.js
+++ script/mobile-action.js
@@ -1,6 +1,7 @@
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
   // true for mobile device
   console.log("mobile device");
+<<<<<<< HEAD
   document.body.onload = themeCheck();
   var gen = document.getElementById('s-general');
   gen.href = "style/general-mobile.css";
@@ -140,4 +141,11 @@ else {
 }
 }
 document.getElementById('inp').addEventListener("click", topping);
+=======
+  var heade = document.getElementById('head');
+  heade.innerHTML = "<link rel='stylesheet' href='style/scrollbar.css'><br><link rel='stylesheet' href='style/texte-mobile.css'><br><link rel='stylesheet' href='style/general-mobile.css'><br><title>Accueil</title>"
+}else{
+  // false for not mobile device
+  console.log("not mobile device");
+>>>>>>> parent of c4c8ed0 (trying dark mode)
 }
